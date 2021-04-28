1ee7b8f6884221d23e3b14ba6890b927becf1b57
林劭軒 <twbd723@gmail.com>
2021-04-28 14:43:27 +0800

    0428

--- Guo/2321_I2P(II)2021_Kuo_hw3/13182.h
+++ Guo/2321_I2P(II)2021_Kuo_hw3/13182.h
@@ -39,10 +39,99 @@ private:
 };
 
 Guest::Guest(){
-
+	name = "";
+	money = 0;
+	skill = 0;
 }
 Guest::Guest(string s, int m, int ski){
-	
+	name = s;
+	money = m;
+	skill = ski;
+}
+Guest::~Guest() = default;
+string Guest::get_name(){
+	return name;
+}
+int Guest::get_money(){
+	return money;
+}
+int Guest::get_skill(){
+	return skill;
+}
+void Guest::Win(int m){
+	money += m;
+}
+
+Casino::Casino(){
+	fee = 0;
+	guest_num = 0;
+	income = 0;
+	list_num = 0;
+}
+Casino::~Casino(){
+	for(int i = 0; i < guest_num; i++) if(guest[i]) delete guest[i];
+}
+void Casino::Enterance(int f){
+	fee = f;
+}
+void Casino::GuestEnter(string s, int m, int ski){
+	// Check if is in blacklist
+	for(int i = 0; i < list_num; i++){
+		if(s == blacklist[i]) return;
+	}
+	// Check if is in Casino
+	for(int i = 0; i < guest_num; i++){
+		if(guest[i] && s == guest[i]->get_name()) return;
+	}
+	// If no money to pay fee
+	if(m < fee){
+		income += m;
+		blacklist[list_num++] = s;
+		return;
+	}
+	// Else Add to guest
+	Guest *new_guest = new Guest(s, m-fee, ski);
+	for(int i = 0; i < guest_num; i++){
+		if(!guest[i]){
+			guest[i] = new_guest;
+			return;
+		}
+	}
+	guest[guest_num++] = new_guest;
+}
+void Casino::Win(string s, int m){
+	// Check if is in blacklist
+	for(int i = 0; i < list_num; i++){
+		if(s == blacklist[i]) return;
+	}
+	// Check if is in Casino
+	for(int i = 0; i < guest_num; i++){
+		if(guest[i] && s == guest[i]->get_name()){
+			// Ban if bankrupt or cheat
+			if(guest[i]->get_money() + m < 0){
+				m = guest[i]->get_money();
+				blacklist[list_num++] = guest[i]->get_name();
+				delete guest[i];
+			} else if(m > 2 * guest[i]->get_skill()){
+				blacklist[list_num++] = guest[i]->get_name();
+				delete guest[i];
+			} else {
+				guest[i]->Win(m);
+			}
+			income -= m;
+			break;
+		}
+	}
+}
+void Casino::EndDay(){
+	for(int i = 0; i < guest_num; i++) if(guest[i]) delete guest[i];
+	guest_num = 0;
+}
+void Casino::Result(){
+	using std::cout;
+	using std::endl;
+	cout << income << endl;
+	for(int i = 0; i < list_num; i++) cout << blacklist[i] << endl;
 }
 
 /*
