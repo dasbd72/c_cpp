ffc588545511d4c5fc1e78602aa1c23a5dc0e0f4
林劭軒 <twbd723@gmail.com>
2021-05-16 22:48:02 +0800

    13131 ac

--- test.cpp
+++ test.cpp
@@ -2,11 +2,10 @@
 using namespace std;
 class myClass{
 public:
-    int a = 1, b = 1, c = 1;
-    myClass():a{2}, b{2}, c{2}{
+    int a;
+    myClass(const int &num):a{num}{
     }
 };
 int main(){
-    myClass obj = {10, 10, 10};
-    cout << obj.a << " " << obj.b << " " << obj.c;
+    myClass *obj;
 }
