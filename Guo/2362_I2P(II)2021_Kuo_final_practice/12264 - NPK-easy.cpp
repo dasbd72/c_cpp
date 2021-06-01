fdbe9b23c7ee264c0d926e074c16cc35c0c00c47
林劭軒 <twbd723@gmail.com>
2021-06-01 15:51:56 +0800

    LAB4

---
+++ Guo/2362_I2P(II)2021_Kuo_final_practice/12264 - NPK-easy.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    string op;
+    cin >> n;
+    while (n--) {
+        cin >> op;
+        if (op == "ENQUEUE") {
+            int x;
+            cin >> x;
+        } else if (op == "DEQUEUE") {
+        }
+    }
+}
