fa22e0795abd90f763388d2c44156626a5cf1247
林劭軒 <twbd723@gmail.com>
2021-06-02 17:42:37 +0800

    12306

--- Guo/2362_I2P(II)2021_Kuo_final_practice/12306 - beat the monster.cpp
+++ Guo/2362_I2P(II)2021_Kuo_final_practice/12306 - beat the monster.cpp
@@ -10,28 +10,29 @@ int main() {
         cin >> dmgi >> hli;
         data.push_back(make_pair(dmgi, hli));
     }
-    /**
-     * @brief BFS's que
-     * @param HP
-     * @param LV
-     * @param MHP
-     * @param steps
-     */
+
     Ans = -1;
-    queue<tuple<int, int, int, int>> tasks;
+    queue<tuple<int, int, int, int>> tasks;  // HP, LV, MHP, STEPS
+    set<tuple<int, int, int>> state;         // HP, LV, MHP
     tasks.push(make_tuple(HP, 1, MHP, 0));
     while (!tasks.empty()) {
         auto [hp, lv, mhp, steps] = tasks.front();
         tasks.pop();
-        // cout << hp << " " << lv << " " << mhp;
+
         if (mhp <= 0) {
             Ans = steps;
             break;
         }
         if (hp <= 0) continue;
 
+        if (state.find(make_tuple(hp, lv, mhp)) != state.end()) continue;
+        state.insert(make_tuple(hp, lv, mhp));
+
+        // cout << hp << " " << lv << " " << mhp << " " << steps << endl;
+
         tasks.push(make_tuple(hp - MDMG, lv, mhp - data[lv].first, steps + 1));
-        tasks.push(make_tuple(hp - MDMG + data[lv].second, lv, mhp, steps + 1));
+        if (hp + data[lv].second < HP)
+            tasks.push(make_tuple(hp - MDMG + data[lv].second, lv, mhp, steps + 1));
         if (lv < L)
             tasks.push(make_tuple(hp - MDMG, lv + 1, mhp, steps + 1));
     }
