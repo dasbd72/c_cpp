11294cb28fc4f9fb6f8ecea960959d6b2ff48785
林劭軒 <twbd723@gmail.com>
2021-06-04 01:02:43 +0800

    Configure

--- test.cpp
+++ test.cpp
@@ -1,44 +1,11 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-class A {
-public:
-    A() {};
-    ~A() {};
-    void f() {
-        cout << "Calling A\n";
-    };
-};
-
-class B : virtual public A {
-public:
-    B() : A() {};
-    ~B() {};
-    void f() {
-        cout << "Calling B\n";
-    };
-};
-
-class C : virtual public A {
-public:
-    C() : A() {};
-    ~C() {};
-    void f() {
-        cout << "Calling C\n";
-    };
-};
-
-class D : public B, public C {
-public:
-    D() : A(), B(), C() {};
-    ~D() {};
-    void f(){
-        cout << "Calling D\n";
-    };
-};
-
 int main() {
-    string s("1234");
-    cout << s.substr(3, -3);
+    map<pair<int, int>, int> mp;
+    mp[make_pair(10, 5)] = 10;
+    mp[make_pair(10, 5)] = 1;
+    mp[make_pair(8, 5)] = 1000;
+    for (auto item : mp) {
+        cout << item.first.first << " " << item.first.second << " " << item.second << endl;
+    }
 }
