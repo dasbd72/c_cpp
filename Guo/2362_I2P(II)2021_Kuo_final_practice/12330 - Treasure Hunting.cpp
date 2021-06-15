1d1a8b44d07d085bcd769288b3d8ba3496fa8345
林劭軒 <twbd723@gmail.com>
2021-06-15 12:13:28 +0800

    12330

--- Guo/2362_I2P(II)2021_Kuo_final_practice/12330 - Treasure Hunting.cpp
+++ Guo/2362_I2P(II)2021_Kuo_final_practice/12330 - Treasure Hunting.cpp
@@ -9,36 +9,36 @@ vector<int> diamondTowns;
 vector<int> G[100005];
 int Dist[100005];
 
-struct node {
-    int id;
-    int dist;
-    node(int id, int l) {
-        this->id = id;
-        this->dist = l;
-    }
-};
-
 int main() {
     int N, M, K;
     cin >> N >> M >> K;
-    for (int i = 0; i < M; i++) {
-        int a, b;
+    for (int i = 0, a, b; i < M; i++) {
         cin >> a >> b;
         G[a].push_back(b);
         G[b].push_back(a);
     }
-    for (int i = 0; i < K; i++) {
-        int x;
+    for (int i = 0, x; i < K; i++) {
         cin >> x;
         diamondTowns.push_back(x);
     }
     fill(Dist, Dist + 100005, -1);
 
-    queue<node> Q;
+    for (int x : diamondTowns) {
+        queue<pair<int, int>> tasks;
 
-    // [TODO] complete the task!
+        tasks.push(make_pair(x, 0));
+        while (!tasks.empty()) {
+            auto [curIdx, curDist] = tasks.front();
+            tasks.pop();
+            if (Dist[curIdx] != -1 && curDist >= Dist[curIdx]) continue;
+            if (Dist[curIdx] == -1 || curDist < Dist[curIdx]) Dist[curIdx] = curDist;
 
-    // Output
+            for (auto nxt : G[curIdx]) {
+                tasks.push(make_pair(nxt, curDist + 1));
+            }
+        }
+    }
+
     for (int i = 1; i <= N; i++) {
         cout << Dist[i] << '\n';
     }
--- test.cpp
+++ test.cpp
@@ -1,5 +1,91 @@
-#include <bits/stdc++.h>
-using namespace std;
+// #include <iostream>
+// #include <string>
+// using namespace std;
+// class Human {
+//    public:
+//     Human(string name, int age) : Name(name), Age(age) {
+//         cout << "Human " << name << " created" << endl;
+//     }
+//     virtual void SelfIntroduce() {
+//         cout << "Hello! My name is " << this->Name;
+//         cout << ", " << this->Age << " years old." << endl;
+//     }
+
+//    protected:
+//     string Name;
+//     int Age;
+// };
+// class Engineer : public Human {
+//    public:
+//     Engineer(string name, int age) : Human(name, age) {}
+//     void SelfIntroduce() override {
+//         cout << "HAHAHAHAHA" << endl;
+//     }
+//     virtual void WriteCode() {
+//         cout << "Coding..." << endl;
+//     }
+// };
+// class Poorgramer : public Engineer {
+//    public:
+//     Poorgramer(string name, int age) : Engineer(name, age) {}
+//     virtual void SelfIntroduce() {
+//         cout << "Hello! My name is " << this->Name;
+//         cout << ", " << this->Age << " years old." << endl;
+//         cout << "As a poorgramer, I have to write code day and night." << endl;
+//     }
+// };
+// int main() {
+//     Poorgramer Mike("Mike", 18);
+//     Engineer *human = &Mike;
+//     Mike.SelfIntroduce();
+//     Mike.WriteCode();
+//     cout << "-----------" << endl;
+//     human->SelfIntroduce();
+//     return 0;
+// }
+
+// #include <iostream>
+// class Foo {
+//    public:
+//     void print() {
+//         std::cout << "Foo\n";
+//     }
+// };
+// class Bar : public Foo {
+//    public:
+//     void print() {
+//         std::cout << "Bar\n";
+//     }
+// };
+// int main() {
+//     Foo a;
+//     a.print();
+//     Bar b;
+//     b.print();
+//     Foo* c = new Bar();
+//     c->print();
+//     return 0;
+// }
+
+#include <cmath>
+#include <iostream>
+// template <typename T, class Func>
+void ForEach(int* _begin, int* _end, void (*_func)(int&)) {
+    for (; _begin != _end; _begin++) {
+        _func(*_begin);
+    }
+}
+struct Power {
+    int exp = 2;
+    void operator()(int& base) {
+        base = std::pow(base, exp);
+    }
+};
+void func(int& base) {
+    base = std::pow(base, 2);
+}
 int main() {
-    for (int i = 1; i <= 4; i <<= 1) cout << i;
+    int A[] = {1, 2, 3, 4, 5};
+    ForEach(A, A + 5, func);
+    return 0;
 }
