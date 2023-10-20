96e38912bf6419f251f40891941adfc9a8086968
dasbd72 <twbd723@gmail.com>
2023-10-20 10:08:00 +0800

    bs special case

--- CP11210/utils/binary-search.cpp
+++ CP11210/utils/binary-search.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 template <class Ty, class FuncTy>
 pair<Ty, Ty> binarySearch(Ty L, Ty R, FuncTy check) {
+    if (check(R) == true)
+        return {R, R + 1};
+    if (check(L) == false)
+        return {L - 1, L};
     while (L + 1 < R) {
         Ty Mid = L + (R - L) / 2;
         if (check(Mid))
