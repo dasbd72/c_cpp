6cd471f43da5c03a046c7e61bd77cb6a69a53ea1
dasbd72 <twbd723@gmail.com>
2023-11-24 21:07:06 +0800

    script

--- CP11210/cppgen.sh
+++ CP11210/cppgen.sh
@@ -10,3 +10,4 @@ int main() {
     solve();
     return 0;
 }" > $(echo "$1" | sed 's/ /-/g' | awk '{$0=$0 ".cpp" ; print tolower(substr($0, 0, length($0) ))}' )
+touch $(echo "$1" | sed 's/ /-/g' | awk '{$0=$0 ".in" ; print tolower(substr($0, 0, length($0) ))}' )
