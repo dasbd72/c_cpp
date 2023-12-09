22b332005c7fa3aebbf470fa2004a243ef534f1c
dasbd72 <twbd723@gmail.com>
2023-12-09 23:37:13 +0800

    disjoint-set

---
+++ CP11210/utils/disjoint-set.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class disjoint_set {
+    vector<int> parent;
+    vector<int> rank;
+
+   public:
+    disjoint_set(int n) : parent(n), rank(n) {
+        for (int i = 0; i < n; i++) {
+            parent[i] = 0;
+            rank[i] = 1;
+        }
+    }
+    int find_root(int x) {
+        int px = parent[x];
+        if (x == px)
+            return x;
+        return parent[x] = find_root(px);
+    }
+    bool same(int a, int b) {
+        return find_root(a) == find_root(b);
+    }
+    int union_basic(int a, int b) {
+        return parent[find_root(a)] = find_root(b);
+    }
+    int union_by_rank(int a, int b) {
+        int ra = find_root(a), rb = find_root(b);
+        if (rank[ra] > rank[rb])
+            swap(ra, rb);
+        if (rank[ra] == rank[rb])
+            ++rank[rb];
+        return parent[ra] = rb;
+    }
+};
