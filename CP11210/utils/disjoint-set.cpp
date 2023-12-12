64f00f8a9b7da3b3777c3612459412c9b5d5b9a3
dasbd72 <twbd723@gmail.com>
2023-12-12 14:18:31 +0800

    disjoint set union by rank

--- CP11210/utils/disjoint-set.cpp
+++ CP11210/utils/disjoint-set.cpp
@@ -3,13 +3,11 @@ using namespace std;
 
 class disjoint_set {
     vector<int> parent;
-    vector<int> rank;
 
    public:
-    disjoint_set(int n) : parent(n), rank(n) {
+    disjoint_set(int n) : parent(n) {
         for (int i = 0; i < n; i++) {
             parent[i] = i;
-            rank[i] = 1;
         }
     }
     int find_root(int x) {
@@ -23,6 +21,27 @@ class disjoint_set {
     }
     int union_basic(int a, int b) {
         return parent[find_root(a)] = find_root(b);
+    }
+};
+class disjoint_set_rank {
+    vector<int> parent;
+    vector<int> rank;
+
+   public:
+    disjoint_set_rank(int n) : parent(n), rank(n) {
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+    }
+    int find_root(int x) {
+        int px = parent[x];
+        if (x == px)
+            return x;
+        return parent[x] = find_root(px);
+    }
+    bool same(int a, int b) {
+        return find_root(a) == find_root(b);
     }
     int union_by_rank(int a, int b) {
         int ra = find_root(a), rb = find_root(b);
