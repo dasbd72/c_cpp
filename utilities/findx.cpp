e1eb80bbe1d8985d6d63cab6b319ede7dc16cd00
dasbd72 <twbd723@gmail.com>
2025-04-13 16:15:39 +0800

    chore: removed redundant

--- utilities/findx.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main() {
-    for (int x = 1; x <= 1800; x++) {
-        if ((x - 3) % 8 == 0 && (x - 6) % 9 == 0 && (x - 7) % 25 == 0)
-            cout << x << endl;
-    }
-}
